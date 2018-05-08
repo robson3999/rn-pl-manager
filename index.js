0e0a96d89600eddd835e4c4484c0a96b20e96740
Robert Głód <robertglod104@gmail.com>
2018-05-08 14:42:00 +0200

    added dynamically changing server IP address - via UDP broadcast

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+const dgram = require('react-native-udp')
+
+const enc = new TextDecoder("utf-8");
+let serverIP = '';
+const socket = dgram.createSocket('udp4')
+socket.bind(9999)
+socket.once('listening', function() {
+  console.log('listening')
+})
+
+socket.once('message', function(msg, rinfo) {
+  serverIP = rinfo.address
+  global.SERVERIP = `http://${serverIP}:8080` || global.SERVERIP
+})
+
 AppRegistry.registerComponent('playlistManager', () => App);
